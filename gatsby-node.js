3098ee908c61b31536d223e8a16321584a149ec3
Rafal Wilinski <raf.wilinski@gmail.com>
2020-03-31 20:52:25 +0200

    Add creating nodes from AppSync data source

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,92 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const axios = require("axios")
+const graphqltag = require("graphql-tag")
+const gql = require("graphql")
+const config = require("./src/aws-exports")
+const { print } = gql
 
-// You can delete this file if you're not using it
+const getListings = `
+query ListListings(
+  $filter: ModelListingFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listListings(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      position
+      description
+      responsibilities
+      requirements
+      timezone
+      tags
+      location
+      applyLink
+      isPromoted
+      salary {
+        perYearFrom
+        perYearTo
+        perHourFrom
+        perHourTo
+      }
+      category {
+        name
+        description
+      }
+      company {
+        id
+        name
+        logo
+        webpage
+        description
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+      expiresAt
+      closedAt
+    }
+    nextToken
+  }
+}
+`
+
+exports.sourceNodes = async ({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) => {
+  const { createNode } = actions
+  const imageKeys = []
+  let authorImages = []
+
+  const getSettingsData = await axios({
+    url: config.aws_appsync_graphqlEndpoint,
+    method: "post",
+    headers: {
+      "x-api-key": config.aws_appsync_apiKey,
+    },
+    data: {
+      query: print(graphqltag(getListings)),
+    },
+  })
+
+  const listings = getSettingsData.data.data.listListings.items
+  const slugs = listings.map(listing => listing.id)
+
+  const slugData = {
+    key: "page-slugs",
+    data: slugs.length ? slugs : "none",
+  }
+  const slugNodeMeta = {
+    id: createNodeId(`my-data-${slugData.key}`),
+    parent: null,
+    children: [],
+    internal: {
+      type: `Slugs`,
+      contentDigest: createContentDigest(slugData),
+    },
+  }
+  const slugNode = Object.assign({}, slugData, slugNodeMeta)
+  createNode(slugNode)
+}
