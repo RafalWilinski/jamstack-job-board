986477faa614f4e60dffcd0518a2aabd1a6a001a
Rafal Wilinski <raf.wilinski@gmail.com>
2020-03-31 12:38:22 +0200

    Extract icons

---
+++ src/components/icons/Edit.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default () => (
+  <svg
+    className="-ml-1 mr-2 h-5 w-5 text-gray-500"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+  >
+    <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+  </svg>
+)
