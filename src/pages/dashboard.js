a1ffa0725904d4ac79290f9fdeb78653a5ff5851
Rafal Wilinski <raf.wilinski@gmail.com>
2020-04-15 14:17:30 +0200

    Change auth a bit

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,9 +1,21 @@
 import React from "react"
-import { withAuthenticator } from "aws-amplify-react"
+import {
+  AmplifyAuthenticator,
+  AmplifySignIn,
+  AmplifyGoogleButton,
+} from "@aws-amplify/ui-react"
 import "@aws-amplify/ui/dist/style.css"
 
 const Dashboard = () => {
-  return <div>Dashboard!</div>
+  return (
+    <AmplifyAuthenticator>
+      <AmplifyGoogleButton
+        slot="sign-in"
+        clientId="187224547248-l63a4lld0b7b8s3gos7taf149f1fa8rn.apps.googleusercontent.com"
+      ></AmplifyGoogleButton>
+      <div>Dashboard!</div>
+    </AmplifyAuthenticator>
+  )
 }
 
-export default withAuthenticator(Dashboard)
+export default Dashboard
