ee9ffac808116e011a8cc6c9031d15bb2f426277
Katarzyna Stoklosa <katarzyna.a.stoklosa@gmail.com>
2020-04-24 00:09:32 +0200

    Make login/register site responsive

--- src/components/RegisterPage.tsx
+++ src/components/RegisterPage.tsx
@@ -15,12 +15,12 @@ const RegisterPage: React.FC<RegisterPageProps> = (props: RegisterPageProps) => 
   return (
     <>
       <nav className="px-8 py-2 w-full bg-gray-800">
-        <span className="text-3xl pl-16 bg-gray-800 text-white font-bold font-black">
+        <span className="text-sm text-center md:text-3xl md:pl-16 bg-gray-800 text-white font-bold font-black">
           JAMStack Job Board
         </span>
       </nav>
       <div className="lg:flex lg:items-center lg:justify-between shadow-md p-5 rounded-lg mb-4">
-        <div className="flex-col bg-white  w-full h-full min-h-screen rounded-md lg:w-1/2 justify-center items-start pb-24 px-10">
+        <div className="hidden lg:block flex-col bg-white  w-full h-full rounded-md lg:w-1/2 justify-center items-start pb-24 px-10">
           <div className="container bg-white rounded-md mt-16 h-100 mx-auto md:pt-4 px-24">
             <Description
               title="Lorem Ipsum"
@@ -48,7 +48,7 @@ const RegisterPage: React.FC<RegisterPageProps> = (props: RegisterPageProps) => 
                 <AmplifyFederatedButtons slot="sign-in" />
                 <AmplifyGoogleButton slot="sign-in" />
               </AmplifyAuthenticator>
-              <p className="leading-normal font-sans font-thin text-sm md:text-m mb-8 text-justify md:text-left">
+              <p className="text-xs leading-normal font-sans font-thin md:text-m mb-8 text-justify md:text-left">
                 By clicking continue, you agree to our Terms of Service and
                 Privacy Policy.
               </p>
