2977fc5183191721a061bb46efff30f561e7bd4d
Katarzyna Stoklosa <katarzyna.a.stoklosa@gmail.com>
2020-05-30 17:46:31 +0200

    Make template for /jobform page
    +add extra icon
    +make separate component for <input> sections

---
+++ src/components/icons/Left.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default () => (
+  <svg
+    className="-ml-1 mr-2 h-5 w-5 text-gray-500"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+  >
+    <path d="M7.05 9.293L6.343 10 12 15.657l1.414-1.414L9.172 10l4.242-4.243L12 4.343z" />
+  </svg>
+)
